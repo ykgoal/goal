dfd957d803dc2223c55d460ab7a8b74cbbc4f8bb
ykgoal <ykgoal@gmail.com>
2025-01-28 12:44:36 +0800

    first project

---
+++ goal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for goal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goal.settings')
+
+application = get_asgi_application()
